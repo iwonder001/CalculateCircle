4fa544b7f6a584e2edf29b7d2c62099deaf9eece
Kristen Carethers <kcarethers@gmail.com>
2016-09-28 16:06:48 -0400

    formatted and cleaned up code in main app.

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,51 +3,54 @@ import java.util.Scanner;
 public class MainApp {
 
 	public static void main(String[] args) {
-		//set up continue
+		// set up continue
 		String inputContinue;
-		boolean userContinue =true;
-		
-		//Welcome Statement
+		boolean userContinue = true;
+
+		// Welcome Statement
 		System.out.println("Welcome to the Circle Tester");
-		
-		//Get user radius input 
+
+		// Get user radius input
 		Scanner input = new Scanner(System.in);
-			
-		//set up a loop to output circumference and area after user enters radius
-		
-		while (userContinue){
+
+		 /* set up a loop to output circumference and area after user enters
+		  radius*/
+
+		while (userContinue) {
 			System.out.print("Enter a radius: ");
 			double inputRadius = input.nextDouble();
-			//make circle object in the main app to call the methods on
-			Circle c = new Circle (inputRadius);
+			// make circle object in the main app to call the methods on
+			Circle c = new Circle(inputRadius);
 			Circle.getObjectCount();
-			 
-			//call the getFormattedCircumference method and pass through the circle object
+
+			/* call the getFormattedCircumference method and pass through the
+			  circle object*/
+			
 			System.out.println("Circumference: " + c.getFormattedCircumference());
+
+			/* call the getFormattedArea method and pass through the circle
+			  object */
 			
-			//call the getFormattedArea method and pass through the circle object
 			System.out.println("Area: " + c.getFormattedArea());
-			
-			
-			
-			//As if continue and capture user input
+
+			// As if continue and capture user input
 			System.out.println("Continue? (y/n): ");
-			/*scanning for an answer. 
-			Taking input from the scanner and putting it into inputContinue */
+			/*scanning for an answer. Taking input from the scanner and putting
+			  it into inputContinue*/
 			inputContinue = input.nextLine();
 			inputContinue = input.nextLine();
-			
-			if(inputContinue.equalsIgnoreCase("y")) {
+
+			if (inputContinue.equalsIgnoreCase("y")) {
 				userContinue = true;
 			} else {
 				userContinue = false;
-				//System.out.println("Goodbye.  You created " + j + " Circle object(s).");
-			}//if/else loop close
-			//System.out.println("Goodbye.  You created " + j + " Circle object(s).");
-		}//close while loop
-		
+				
+			} // if/else loop close
+				
+		} // close while loop
+
 		System.out.println("Goodbye.  You created " + Circle.getObjectCount() + " Circle object(s).");
 
-	}//close psvm
+	}// close psvm
 
-}//close MainApp class
+}// close MainApp class
