15d93a49d5ae1773cd787cb2785bd02506e3542c
Kristen Carethers <kcarethers@gmail.com>
2016-09-28 15:52:36 -0400

    cleaned up formatting in Circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,57 +1,51 @@
 
 public class Circle {
-	
-	//declare instance variables
-	//public double circumference;
-	//public double area;
-	public double radius;
-	public static int counter=0;
-	int x =0;
 
-	//create constructor
-	public Circle (double rad) {
+	// declare instance variables
+	public double radius;
+	public static int counter = 0;
+
+	// create constructor
+	public Circle(double rad) {
 		radius = rad;
 		counter++;
 	}
-	//get methods for area
-	public double getArea(){
+
+	// get methods for area
+	public double getArea() {
 		double area = radius * radius * Math.PI;
 		return area;
 	}
-	//format area return to 2 decimal points
-	private String formatNumber (double x){
-		String formattedNumber=String.format("%, .2f", x);
+
+	// format area return to 2 decimal points
+	private String formatNumber(double x) {
+		String formattedNumber = String.format("%, .2f", x);
 		return formattedNumber;
 	}
-	//pass the getArea method into the formatting
-	public String getFormattedArea(){
-		String myArea= formatNumber(getArea());
+
+	// pass the getArea method into the formatting
+	public String getFormattedArea() {
+		String myArea = formatNumber(getArea());
 		return myArea;
-		
+
 	}
-	//get method for circumference
+
+	// get method for circumference
 	public double getCircumference() {
 		double circumference = 2 * Math.PI * radius;
 		return circumference;
 	}
-	public String getFormattedCircumference(){
+
+	public String getFormattedCircumference() {
 		String myCircumference = formatNumber(getCircumference());
 		return myCircumference;
 	}
-	public static int getObjectCount(){
+
+	public static int getObjectCount() {
 		return counter;
-	//need to increase counter. put this under the created circle object in the main app
+		/* need to increase counter in the circle constructor. 
+		 * put this under the created circle object in
+		 the main app*/
 	}
-	
-	
-	
-	//public Circle (double radius);
-	//public double getCircumference();
-	//public String getFormattedCircumference();
-	//public double getArea()
-	//public String getFormattedArea()
-	//private String formatNumber (double x)
-	//public static int getObjectCount()
-	
-	
-}
+
+}//close Circle class
