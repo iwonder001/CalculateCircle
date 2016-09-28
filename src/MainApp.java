b0fc6398dc85ad42ed7527ad95a6f7fb7e01def4
Kristen Carethers <kcarethers@gmail.com>
2016-09-28 13:35:37 -0400

    Asked user if want to continue and captured answer.

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,16 +3,40 @@ import java.util.Scanner;
 public class MainApp {
 
 	public static void main(String[] args) {
+		//set up continue
+		String inputContinue;
+		boolean userContinue =true;
+		
 		//Welcome Statement
 		System.out.println("Welcome to the Circle Tester");
 		
 		//Get user radius input 
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a radius");
-		String inputRadius = input.nextLine();
+//		System.out.println("Enter a radius");
+//		String inputRadius = input.nextLine();
 		
 		//make circle object in the main app to call the methods on
 		Circle c = new Circle ();
+		
+		//set up a loop to output circumference and area after user enters radius
+		
+		while (userContinue){
+			System.out.println("Enter a radius");
+			String inputRadius = input.nextLine();
+			
+			//As if continue and capture user input
+			System.out.println("Continue? (y/n): ");
+			/*scanning for an answer. 
+			Taking input from the scanner and putting it into inputContinue */
+			inputContinue = input.nextLine();
+			//
+			if(inputContinue.equals("yes")) {
+				userContinue = true;
+			} else {
+				userContinue = false;
+			}
+			
+		}//close while loop
 		
 		
 
