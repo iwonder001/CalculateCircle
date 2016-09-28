1e4e23e6a855351f076eb6e28b0e504c7265aa7b
Kristen Carethers <kcarethers@gmail.com>
2016-09-28 13:08:05 -0400

    sysout asking for user input and capturing input

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class MainApp {
 
 	public static void main(String[] args) {
-		
+		//Welcome Statement
 		System.out.println("Welcome to the Circle Tester");
 		
+		//Get user radius input 
 		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a radius");
 		String inputRadius = input.nextLine();
 		
 		//make circle object in the main app to call the methods on
